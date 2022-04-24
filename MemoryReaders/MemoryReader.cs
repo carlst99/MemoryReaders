ab23549e8e9ea846c5503405a745945d46e85cd8
Carl Stephens <carlst99@outlook.co.nz>
2022-04-25 11:13:19 +1200

    Refactor item-search based methods to separate files

--- MemoryReaders/MemoryReader.cs
+++ MemoryReaders/MemoryReader.cs
@@ -9,7 +9,7 @@ namespace MemoryReaders
     /// on performance and minimal or zero heap allocations.
     /// </summary>
     /// <typeparam name="T">The type of the read-only memory.</typeparam>
-    public struct MemoryReader<T> where T : unmanaged, IEquatable<T>
+    public partial struct MemoryReader<T> where T : unmanaged, IEquatable<T>
     {
         /// <summary>
         /// Gets the underlying <see cref="ReadOnlyMemory{T}"/> for the reader.
@@ -166,44 +166,6 @@ namespace MemoryReaders
             }
 
             value = Memory.Span[Consumed++];
-            return true;
-        }
-
-        /// <summary>
-        /// Advances to the given delimiter, if found, and optionally advances past it.
-        /// </summary>
-        /// <param name="delimiter">The delimiter to search for.</param>
-        /// <param name="advancePastDelimiter"><c>True</c> to move past the <paramref name="delimiter"/>, if found.</param>
-        /// <returns><c>True</c> if the given <paramref name="delimiter"/> was found, otherwise <c>False</c>.</returns>
-        public bool TryAdvanceTo(T delimiter, bool advancePastDelimiter = true)
-        {
-            if (End)
-                return false;
-
-            int index = Memory.Span[Consumed..].IndexOf(delimiter);
-            if (index == -1)
-                return false;
-
-            Consumed = advancePastDelimiter ? index + 1 : index;
-            return true;
-        }
-
-        /// <summary>
-        /// Advances to the given delimiter, if found, and optionally advances past it.
-        /// </summary>
-        /// <param name="delimiter">The delimiter to search for.</param>
-        /// <param name="advancePastDelimiter"><c>True</c> to move past the <paramref name="delimiter"/>, if found.</param>
-        /// <returns><c>True</c> if the given <paramref name="delimiter"/> was found, otherwise <c>False</c>.</returns>
-        public bool TryAdvanceTo(ReadOnlySpan<T> delimiter, bool advancePastDelimiter = true)
-        {
-            if (End)
-                return false;
-
-            int index = Memory.Span[Consumed..].IndexOf(delimiter);
-            if (index == -1)
-                return false;
-
-            Consumed = advancePastDelimiter ? index + delimiter.Length : index;
             return true;
         }
     }
---
+++ MemoryReaders/SpanReader.Search.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MemoryReaders
+{
+    public ref partial struct SpanReader<T> where T : unmanaged, IEquatable<T>
+    {
+        /// <summary>
+        /// Advances to the given delimiter, if found, and optionally advances past it.
+        /// </summary>
+        /// <param name="delimiter">The delimiter to search for.</param>
+        /// <param name="advancePastDelimiter"><c>True</c> to move past the <paramref name="delimiter"/>, if found.</param>
+        /// <returns><c>True</c> if the given <paramref name="delimiter"/> was found, otherwise <c>False</c>.</returns>
+        public bool TryAdvanceTo(T delimiter, bool advancePastDelimiter = true)
+        {
+            if (End)
+                return false;
+
+            int index = Span[Consumed..].IndexOf(delimiter);
+            if (index == -1)
+                return false;
+
+            Consumed = advancePastDelimiter ? index + 1 : index;
+            return true;
+        }
+
+        /// <summary>
+        /// Advances to the given delimiter, if found, and optionally advances past it.
+        /// </summary>
+        /// <param name="delimiter">The delimiter to search for.</param>
+        /// <param name="advancePastDelimiter"><c>True</c> to move past the <paramref name="delimiter"/>, if found.</param>
+        /// <returns><c>True</c> if the given <paramref name="delimiter"/> was found, otherwise <c>False</c>.</returns>
+        public bool TryAdvanceTo(ReadOnlySpan<T> delimiter, bool advancePastDelimiter = true)
+        {
+            if (End)
+                return false;
+
+            int index = Span[Consumed..].IndexOf(delimiter);
+            if (index == -1)
+                return false;
+
+            Consumed = advancePastDelimiter ? index + delimiter.Length : index;
+            return true;
+        }
+    }
+}
