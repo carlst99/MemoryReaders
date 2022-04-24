c9bc9b2273e524600a43b49b7b725f1e8fb23420
Carl Stephens <carlst99@outlook.co.nz>
2022-04-24 14:26:27 +1200

    Add Slice tests

---
+++ MemoryReaders.Tests/MemoryReader/Slice.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace MemoryReaders.Tests.MemoryReader;
+
+public class Slice
+{
+    [Fact]
+    public void WithoutStart()
+    {
+        MemoryReader<char> reader = Constants.DefaultMemoryReader;
+        reader.Advance(2);
+
+        MemoryReader<char> slice = reader.Slice(4);
+        Assert.Equal(4, slice.Remaining);
+        Assert.Equal(reader.Memory.Span[2], slice.Memory.Span[0]);
+        Assert.Equal(reader.Memory.Span[5], slice.Memory.Span[3]);
+    }
+
+    [Fact]
+    public void WithStart()
+    {
+        MemoryReader<char> reader = Constants.DefaultMemoryReader;
+
+        MemoryReader<char> slice = reader.Slice(2, 4);
+        Assert.Equal(4, slice.Remaining);
+        Assert.Equal(reader.Memory.Span[2], slice.Memory.Span[0]);
+        Assert.Equal(reader.Memory.Span[5], slice.Memory.Span[3]);
+        Assert.Equal(reader.Memory.Span[5], slice.Memory.Span[3]);
+    }
+}
---
+++ MemoryReaders.Tests/SpanReader/Slice.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace MemoryReaders.Tests.SpanReader;
+
+public class Slice
+{
+    [Fact]
+    public void WithoutStart()
+    {
+        SpanReader<char> reader = Constants.DefaultSpanReader;
+        reader.Advance(2);
+
+        SpanReader<char> slice = reader.Slice(4);
+        Assert.Equal(4, slice.Remaining);
+        Assert.Equal(reader.Span[2], slice.Span[0]);
+        Assert.Equal(reader.Span[5], slice.Span[3]);
+    }
+
+    [Fact]
+    public void WithStart()
+    {
+        SpanReader<char> reader = Constants.DefaultSpanReader;
+
+        SpanReader<char> slice = reader.Slice(2, 4);
+        Assert.Equal(4, slice.Remaining);
+        Assert.Equal(reader.Span[2], slice.Span[0]);
+        Assert.Equal(reader.Span[5], slice.Span[3]);
+    }
+}
