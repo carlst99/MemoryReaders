a7aaeb6f03063d3615c5a98ade570df9cdfab8b3
Carl Stephens <carlst99@outlook.co.nz>
2022-11-12 09:57:24 +1300

    Add large data benchmarks for TryReadTo.cs

--- MemoryReaders.Benchmarks/Benchmarks/TryReadTo.cs
+++ MemoryReaders.Benchmarks/Benchmarks/TryReadTo.cs
@@ -1,12 +1,16 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
 using System;
 using System.Buffers;
 
 namespace MemoryReaders.Benchmarks.Benchmarks;
 
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+[CategoriesColumn]
 public class TryReadTo
 {
     [Benchmark(Baseline = true)]
+    [BenchmarkCategory("ShortData")]
     public ReadOnlySpan<char> UsingSequenceReader()
     {
         SequenceReader<char> reader = Constants.GetDefaultSequenceReader();
@@ -15,6 +19,7 @@ public class TryReadTo
     }
 
     [Benchmark]
+    [BenchmarkCategory("ShortData")]
     public ReadOnlyMemory<char> UsingMemoryReader()
     {
         MemoryReader<char> reader = Constants.GetDefaultMemoryReader();
@@ -23,10 +28,38 @@ public class TryReadTo
     }
 
     [Benchmark]
+    [BenchmarkCategory("ShortData")]
     public ReadOnlySpan<char> UsingSpanReader()
     {
         SpanReader<char> reader = Constants.GetDefaultSpanReader();
         reader.TryReadTo(out ReadOnlySpan<char> span, Constants.DataString[5]);
+        return span;
+    }
+
+    [Benchmark(Baseline = true)]
+    [BenchmarkCategory("LongData")]
+    public ReadOnlySpan<char> UsingLongSequenceReader()
+    {
+        SequenceReader<char> reader = Constants.GetDefaultLongSequenceReader();
+        reader.TryReadTo(out ReadOnlySpan<char> span, Constants.LongDataUniqueCharacter);
+        return span;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("LongData")]
+    public ReadOnlyMemory<char> UsingLongMemoryReader()
+    {
+        MemoryReader<char> reader = Constants.GetDefaultLongMemoryReader();
+        reader.TryReadTo(out ReadOnlyMemory<char> memory, Constants.LongDataUniqueCharacter);
+        return memory;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("LongData")]
+    public ReadOnlySpan<char> UsingLongSpanReader()
+    {
+        SpanReader<char> reader = Constants.GetDefaultLongSpanReader();
+        reader.TryReadTo(out ReadOnlySpan<char> span, Constants.LongDataUniqueCharacter);
         return span;
     }
 }
--- MemoryReaders.Benchmarks/Constants.cs
+++ MemoryReaders.Benchmarks/Constants.cs
@@ -1,23 +1,59 @@
 ﻿using System;
 using System.Buffers;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace MemoryReaders.Benchmarks;
 
 public static class Constants
 {
-    // Please, do not change this string. You will break many the benchmarks
+    // Please, do not change this string. You will break many of the benchmarks
     public const string DataString = "The quick brown fox jumped over the lazy dog";
+    public const char LongDataUniqueCharacter = '1';
+
+    public static readonly ReadOnlyMemory<char> DataStringMemory;
+    public static readonly ReadOnlySequence<char> DataStringSequence;
+
+    public static readonly string LongDataString;
+    public static readonly ReadOnlyMemory<char> LongDataStringMemory;
+    public static readonly ReadOnlySequence<char> LongDataStringSequence;
+
+    static Constants()
+    {
+        DataStringMemory = DataString.AsMemory();
+        DataStringSequence = new ReadOnlySequence<char>(DataStringMemory);
+
+        StringBuilder sb = new(DataString.Length * 100 + 1);
+        for (int i = 0; i < 100; i++)
+            sb.Append(DataString);
+        sb.Append(LongDataUniqueCharacter);
+
+        LongDataString = sb.ToString();
+        LongDataStringMemory = LongDataString.AsMemory();
+        LongDataStringSequence = new ReadOnlySequence<char>(LongDataStringMemory);
+    }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static MemoryReader<char> GetDefaultMemoryReader()
-        => new(DataString.AsMemory());
+        => new(DataStringMemory);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static SequenceReader<char> GetDefaultSequenceReader()
-        => new(new ReadOnlySequence<char>(DataString.AsMemory()));
+        => new(DataStringSequence);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static SpanReader<char> GetDefaultSpanReader()
         => new(DataString);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static MemoryReader<char> GetDefaultLongMemoryReader()
+        => new(LongDataStringMemory);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static SequenceReader<char> GetDefaultLongSequenceReader()
+        => new(LongDataStringSequence);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static SpanReader<char> GetDefaultLongSpanReader()
+        => new(LongDataString);
 }
